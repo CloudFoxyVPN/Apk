60efde98102287278e19f9d51d6b6d9e0926a692
CloudFoxyVPN <133342273+CloudFoxyVPN@users.noreply.github.com>
2023-05-15 00:06:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,225 @@
+{
+"Version": "02",
+"Servers": [
+{
+		"Name": "🟣 Vivo AmazonS3 [1] 🚀",
+        "Type": "ViP",
+        "Server": "13.227.126.65",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET / HTTP/1.1[crlf]Host: d3gpz867aocofy.cloudfront.net[crlf]Connection: Upgrade[crlf]Upgrade: Websocket[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+    	"Name": "🟣 Vivo AmazonS3 [2] 🚀",
+        "Type": "ViP",
+        "Server": "13.227.126.65",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET wss://d3gpz867aocofy.cloudfront.net HTTP/1.1[crlf]Host: recarga.vivo.com.br[crlf]Upgrade: Websocket[crlf]Connection: Upgrade[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+    	"Name": "🟣 Vivo CloudFlare 🌤️",
+        "Type": "ViP",
+        "Server": "104.16.51.111",
+        "Connection": "Direct",
+        "Port": "2052",
+        "Payload": "GET  / HTTP/1.1[crlf]Host: store.cloudfoxy.site[crlf]Upgrade: websocket[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Saldo [1] 🍀",
+        "Type": "ViP",
+        "Server": "104.16.53.111",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "CONNECT / HTTP/1.1[crlf]host: www.c6bank.com.br[crlf][crlf][split][crlf]OPTIONS- // HTTP/1.1[crlf]host: store.cloudfoxy.site[crlf]Expect: 200-continue[crlf]Proxy-Connection: Keep-Alive[crlf]Keep-Alive: timeout=5, max=1000[crlf]Connection: Upgrade[crlf]Upgrade: websocket[crlf]Sec WebSocketExtensions: superspeed[crlf]Last-Modified: Fri, 17 March 2022 04:32:39 GMT[crlf]Server: Qnax[crlf]User-Agent: Mozilla/5.0 (Linux; Android 4.4.2); Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Mobile Safari/537.36 OPR/20.0.1396.72047[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Saldo [2] 🍀",
+        "Type": "ViP",
+        "Server": "104.19.239.25",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "CONNECT / HTTP/1.1[crlf]host: mobile.c6bank.app[crlf][crlf][split][crlf]OPTIONS- // HTTP/1.1[crlf]host: store.cloudfoxy.site[crlf]Expect: 200-continue[crlf]Proxy-Connection: Keep-Alive[crlf]Keep-Alive: timeout=5, max=1000[crlf]Connection: Upgrade[crlf]Upgrade: websocket[crlf]Sec WebSocketExtensions: superspeed[crlf]Last-Modified: Fri, 17 March 2022 04:32:39 GMT[crlf]Server: Qnax[crlf]User-Agent: Mozilla/5.0 (Linux; Android 4.4.2); Nexus 5 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Mobile Safari/537.36 OPR/20.0.1396.72047[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Saldo [3] 🍀",
+        "Type": "ViP",
+        "Server": "104.19.239.25",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET / HTTP/1.1\nHost:cdn.c6bank.com.br\nExpect: 100-continue\n\n[split]\n\nACL /?/ HTTP/1.1\nHost: store.cloudfoxy.site\nExpect: 100-continue\nUpgrade: websocket\nUser-Agent: Googlebot/2.1 (+http://www.google.com/bot.html)\n\n",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Saldo [4] 🍀",
+        "Type": "ViP",
+        "Server": "104.19.239.25",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET / HTTP/1.1[crlf]Host:cdn.c6bank.com.br[crlf]Expect: 100-continue[crlf][crlf][split][crlf][crlf]ACL / HTTP/1.1[crlf]Host: store.cloudfoxy.site[crlf]Upgrade: Websocket[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Expirado [1] ✈️",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "PROPPATCH global-4-lvs-colossus-10.opera-mini.net HTTP/1.0[crlf]Host: global-4-lvs-colossus-10.opera-mini.net[crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+        "Name": "🔵 Tim Expirado [2] ✈️",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "MERGE ok.ru HTTP/1.1[crlf]Host: ok.ru[crlf]Connection: upgrade, Keep-Alive[crlf]Upgrade: HTTP/2.0[crlf]User-Agent: Googlebot/2.1 (+http://www.google.com/bot.html)[crlf]Referer: http://www.google.com[crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+        "User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Pré-Pago [1] ⚡",
+        "Type": "ViP",
+        "Server": "embed.claromusica.com",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET wss://d3gpz867aocofy.cloudfront.net HTTP/1.1[crlf]Host: embed.claromusica.com[crlf]Upgrade: Websocket[crlf]Connection: Upgrade[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Pré-Pago [2] ⚡",
+        "Type": "ViP",
+        "Server": "hmg-embed.claromusica.com",
+        "Connection": "Direct",
+        "Port": "80",
+        "Payload": "GET wss://d3gpz867aocofy.cloudfront.net HTTP/1.1[crlf]Host: hmg-embed.claromusica.com[crlf]Upgrade: Websocket[crlf]Connection: Upgrade[crlf][crlf]",
+        "SNI": "null",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Prezão [1] 🔓",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "SSL",
+        "Port": "443",
+        "Payload": "",
+        "SNI": "api.whatsapp.com",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Prezão [2] 🔓",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "SSL",
+        "Port": "443",
+        "Payload": "",
+        "SNI": "arcade.giphy.com",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Pós-Pago 🌙",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "SSL",
+        "Port": "443",
+        "Payload": "",
+        "SNI": "m.twitter.com",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Controle 🌙",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "SSL",
+        "Port": "443",
+        "Payload": "",
+        "SNI": "m.waze.com",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+},
+{
+		"Name": "🔴 Claro Flex 🌙",
+        "Type": "ViP",
+        "Server": "store.cloudfoxy.site",
+        "Connection": "SSL",
+        "Port": "443",
+        "Payload": "",
+        "SNI": "m.twitter.com",
+        "Proxy": "null",
+        "ProxyPort": "null",
+		"User": "",
+		"Pass": ""
+}
+        ],
+	    "UDP":[
+		{
+			"Name":"Porta UDP 7300",
+			"UDP": "127.0.0.1:7300"
+		},{
+			"Name":"Porta UDP 7305",
+			"UDP": "127.0.0.1:7305"
+		},{
+			"Name":"Porta UDP 7345",
+			"UDP": "127.0.0.1:7245"
+		}
+		]
+}
